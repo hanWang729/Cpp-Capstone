2813f9a51a135cec2edaa3ea885141790a650652
hanWang729 <13240928761@163.com>
2021-08-09 18:54:54 +0000

    Now the player can custimize the difficulties

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,10 @@ int main()
   std::cout << "1. Easy" << std::endl;
   std::cout << "2. Normal (Default)" << std::endl;
   std::cout << "3. Difficult" << std::endl;
+  std::cout << "4. Customization" << std::endl;
   int dif;
-  double speed;
-  int foodnum;
+  double speed = 0.2;
+  int foodnum = 3;
   std::cin >> dif;
   switch (dif)
   {
@@ -27,10 +28,25 @@ int main()
     speed = 0.4;
     foodnum = 1;
     break;
+  case 4:
+    std::cout << "Input the speed of the ghost(0.1 - 2)";
+    std::cin >> speed;
+    if (speed < 0.1)
+      speed = 0.1;
+    else if (speed > 2)
+      speed = 2;
+    std::cout << "Input the number of food every round (1-10)";
+    std::cin >> foodnum;
+    if (foodnum < 1)
+      foodnum = 1;
+    else if (foodnum > 10)
+      foodnum = 10;
+    break;
   default:
     speed = 0.2;
     foodnum = 3;
   }
+  std::cout << "Let's start" << std::endl;
   constexpr std::size_t kFramesPerSecond{60};
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
   constexpr std::size_t kScreenWidth{640};
